daf2ec9d7e931951db69b81abc4c79271acbbb77
hawkinchina <free_hawk@sina.com>
2022-01-04 20:54:39 +0800

    1 file modified

--- src/app/module/user/user.module.ts
+++ src/app/module/user/user.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UserComponent } from './user.component';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [CommonModule],
+  declarations: [UserComponent],
 })
-export class UserModule { }
+export class UserModule {}
